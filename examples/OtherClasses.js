9f291d727c8295fd2b7f7197063d753e6d07f6b7
S1nus <chuckhead7@gmail.com>
2017-02-10 19:21:24 -0500

    BouncingBall is now my resume website lol

--- examples/OtherClasses.js
+++ examples/OtherClasses.js
@@ -4,7 +4,7 @@ function Label(x, y, text, size="16px", font="Emulator", color="limegreen") {
   this.color = color;
   this.font = font;
   this.size = size;
-  
+
   this.draw = function(game) {
     game.ctx.fillStyle = this.color;
     game.ctx.font = this.size + " " + this.font;
@@ -14,13 +14,6 @@ function Label(x, y, text, size="16px", font="Emulator", color="limegreen") {
 }
 Label.prototype = Object.create(Entity.prototype);
 Label.prototype.constructor = Label;
-
-function Brick(x, y) {
-  Entity.call(this, x, y, 81, 20, "rgb("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+")");
-
-}
-Brick.prototype = Object.create(Entity.prototype);
-Brick.prototype.constructor = Brick;
 
 function RandomBall(x, y) {
   Circ.call(this, x, y, 10+Math.floor(Math.random()*40), "rgb("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+")");
@@ -36,3 +29,24 @@ function RandomBall(x, y) {
     }
   };
 }
+RandomBall.prototype = Object.create(Circ.prototype);
+RandomBall.prototype.constructor = RandomBall;
+
+function TexRect(src, x, y) {
+  this.img = new Image();
+  this.img.src = src;
+  this.width = this.img.width;
+  this.height = this.img.height;
+  Entity.call(this, x, y, this.width, this.height);
+
+  this.draw = function(game) {
+    game.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  };
+
+  this.update = function(game) {
+    this.width = this.img.width;
+    this.height = this.img.height;
+  };
+}
+TexRect.prototype = Object.create(Entity.prototype);
+TexRect.prototype.constructor = TexRect;
